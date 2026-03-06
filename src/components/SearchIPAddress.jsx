f9bf83d36ed9d2eef6f987d00c559b791720a99c
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-06 15:35:08 -0500

    style adjustment

--- src/components/SearchIPAddress.jsx
+++ src/components/SearchIPAddress.jsx
@@ -2,31 +2,6 @@ import { useState } from "react";
 import arrowIcon from "../assets/images/icon-arrow.svg";
 import bgImage from "../assets/images/pattern-bg-desktop.png";
 
-// function SearchIpAddress() {
-//   return (
-//     <div
-//       className="bg-cover h-80"
-//       style={{ backgroundImage: `url(${bgImage})` }}
-//     >
-//             <h1 className="text-center text-2xl">IP Address Tracker</h1>
-//
-//       <div className="flex justify-center">
-//                 <label htmlFor="searchIp"></label>
-//
-//         <input className="border" type="search" id="searchIp" />
-//
-//         <button className="h-10 w-10 bg-black border-none">
-//
-//           <img alt="Arrow Icon" src={arrowIcon} className="h-5 w-5" />
-//
-//         </button>
-//
-//       </div>
-//
-//     </div>
-//   );
-// }
-// export default SearchIpAddress;
 
 const SearchIPAddress = ({fetchIp}) => {
   const [query, setQuery] = useState("");
@@ -40,7 +15,17 @@ const SearchIPAddress = ({fetchIp}) => {
   return (
     <div
      className="bg-cover h-80"
-    style={{ backgroundImage: `url(${bgImage})` }}
+    style={{
+       backgroundImage: `url(${bgImage})`,
+  backgroundSize: "cover",
+      backgroundPosition: "top center",
+     backgroundRepeat: "no-repeat",
+     height: "30vh",
+     display: "flex",
+     flexDirection: "column",
+     alignItems: "center",
+     justifyContent: "center",
+  }}
     >  
         <form id="searchForm" onSubmit={handleSubmit}>
       <h1>IP Address Tracker</h1>
