8c548c9b63512a47c46dd6e28a5988e572aaa99c
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-06 16:03:39 -0500

    style adjustment

--- src/components/SearchIPAddress.jsx
+++ src/components/SearchIPAddress.jsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import arrowIcon from "../assets/images/icon-arrow.svg";
 import bgImage from "../assets/images/pattern-bg-desktop.png";
 
-
-const SearchIPAddress = ({fetchIp}) => {
+const SearchIPAddress = ({ fetchIp }) => {
   const [query, setQuery] = useState("");
 
   const handleSubmit = (e) => {
@@ -14,37 +13,38 @@ const SearchIPAddress = ({fetchIp}) => {
 
   return (
     <div
-     className="bg-cover h-80"
-    style={{
-       backgroundImage: `url(${bgImage})`,
-  backgroundSize: "cover",
-      backgroundPosition: "top center",
-     backgroundRepeat: "no-repeat",
-     height: "30vh",
-     display: "flex",
-     flexDirection: "column",
-     alignItems: "center",
-     justifyContent: "center",
-  }}
-    >  
-        <form id="searchForm" onSubmit={handleSubmit}>
-      <h1>IP Address Tracker</h1>
+      className="bg-cover h-80"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "top center",
+        backgroundRepeat: "no-repeat",
+        height: "35vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "2rem",
+        textAlign: "center",
+        justifyContent: "center",
+      }}
+    >
+      <form id="searchForm" onSubmit={handleSubmit}>
+        <h1>IP Address Tracker</h1>
 
-      <div className="searchbox">
-        <input
-          type="text"
-          id="searchInput"
-          placeholder="Search for IP address or domain"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button type="submit" id="submit">
-          <img alt="Arrow Icon" src={arrowIcon} className="h-5 w-5" />
-        </button>
-      </div>
-    </form>
+        <div className="searchbox">
+          <input
+            type="text"
+            id="searchInput"
+            placeholder="Search for IP address or domain"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit" id="submit">
+            <img alt="Arrow Icon" src={arrowIcon} className="h-5 w-5" />
+          </button>
+        </div>
+      </form>
     </div>
-
   );
 };
 
